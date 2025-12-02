4e05785557e180d1edb8d1d135773ff8afba9e80
Suraj Vishwakarma <your-email@example.com>
2025-12-02 15:56:20 +0530

    Initial portfolio upload

---
+++ index.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="style.css">
+<body>
+       <header>
+        <div class="logo">Suraj
+        </div>
+        <div class="humburger" id="humburger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <nav id="nav-links">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#certificates">certificates</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#login">Login</a></li>
+                Chai or Suraj ☕❤️
+            </ul>
+        </nav>
+    </header>
+
+
+    <div class="about-container" id="home">
+        <img src="c:\Users\imdee\OneDrive\Pictures\ss.jpeg" alt="suraj">
+        <div class="about-text">
+            Hi, <span style="font-size: 25px; color: rgb(108, 247, 175);"> I’m Suraj </span> — a passionates <span
+                style="font-size: 25px; color: yellow;"> Frontend Developer </span> who loves turning ideas into clean,
+            responsive, and user-friendly web experiences. I specialize in HTML, CSS, JavaScript, and modern UI design,
+            and I enjoy building websites that are both functional and visually appealing. My focus is on writing clean
+            code, improving performance, and creating smooth interactions that enhance the user experience. I’m
+            constantly learning, exploring new tools, and improving my skills to deliver better and more creative
+            digital solutions.
+            <div class="action-buttons">
+                <a href="#contact" class="hire-btn">Hire Me</a>
+                <a href="resume.pdf" download class="resume-btn">Download Resume</a>
+            </div><br>
+            <div class="footer-social">
+                <div class="social-icons">
+                    <a href="https://github.com/Vishwakarmasuraj333"><img
+                            src="https://img.icons8.com/ios-filled/50/ffffff/github.png" /></a>
+                    <a href="http://linkedin.com/in/suraj-vishwakarma-2ab64436a"><img
+                            src="https://img.icons8.com/ios-filled/50/ffffff/linkedin.png" /></a>
+                    <a href="https://www.instagram.com/invites/contact/?utm_source=ig_contact_invite&utm_medium=copy_link&utm_content=mngzxht"
+                        target="_blank"><img src="https://img.icons8.com/ios-filled/50/ffffff/instagram-new.png" /></a>
+                    <a href="https://www.facebook.com/share/1AWPJbsWya/" target="_blank"><img
+                            src="https://img.icons8.com/ios-filled/50/ffffff/facebook.png" /></a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div id="projects" class="projects-section">
+        <h2>My Projects</h2>
+
+        <div class="projects-grid">
+
+            <div class="project-card">
+                <h3>1. Portfolio Website</h3>
+                <p>A fully responsive personal portfolio built with HTML, CSS & JS.</p>
+            </div>
+
+            <div class="project-card">
+                <h3>2. Weather App</h3>
+                <p>Real-time weather updates using API with clean UI.</p>
+            </div>
+
+            <div class="project-card">
+                <h3>3. To-Do App</h3>
+                <p>A simple task manager with local storage.</p>
+            </div>
+
+            <div class="project-card">
+                <h3>4. Login System</h3>
+                <p>Frontend login UI with validation.</p>
+            </div>
+
+            <div class="project-card">
+                <h3>5. Calculator App</h3>
+                <p>Basic calculator using JavaScript functions.</p>
+            </div>
+
+            <div class="project-card">
+                <h3>6. Responsive Landing Page</h3>
+                <p>Modern landing page with animations.</p>
+            </div>
+
+            <div class="project-card">
+                <h3>7. Quiz App</h3>
+                <p>MCQ quiz app with scoring system.</p>
+            </div>
+
+            <div class="project-card">
+                <h3>8. Music Player UI</h3>
+                <p>A stylish music player design built using pure CSS & JS.</p>
+            </div>
+            <div class="project-card">
+                <h3>9. E-commerce</h3>
+                <p>A stylish online bying & selling built using pure CSS & JS.</p>
+            </div>
+            <div class="project-card">
+                <h3>10. DashBoards</h3>
+                <p>A stylish Dashboard Resuilt BI built using pure CSS & JS.</p>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="skills-container" id="skills">
+        <h2 class="skill-title" style="padding-bottom: 30px;">Skills</h2>
+
+        <div class="skills-grid">
+            <div class="skill">HTML5</div>
+            <div class="skill">CSS3</div>
+            <div class="skill">JavaScript</div>
+            <div class="skill">React JS</div>
+            <div class="skill">Node JS</div>
+            <div class="skill">MongoDB</div>
+            <div class="skill">EXpress js</div>
+            <div class="skill">Git & GitHub</div>
+        </div>
+
+        <h2 class="skill-title" style="padding-bottom: 30px;">Running Practices Skills</h2>
+
+        <div class="skills-grid">
+            <div class="skill">PHP</div>
+            <div class="skill">MySQL</div>
+            <div class="skill">Python</div>
+            <div class="skill">TypeScript</div>
+            <div class="skill">C / C++ / C#</div>
+            <div class="skill">Saas</div>
+        </div>
+    </div>
+
+    <div class="certificate-section">
+        <h2>My Certificates</h2>
+
+        <div class="certificates" id="certificates">
+            <img src="c:\Users\imdee\Downloads\elec certificate.jpeg" alt="Certificate 1">
+            <img src="c:\Users\imdee\Downloads\s2.jpeg" alt="Certificate 2">
+            <img src="c:\Users\imdee\Downloads\simplelearn1.jpeg" alt="Certificate 3">
+            <img src="C:\Users\imdee\Downloads\c4.jpeg" alt="Certificate 4">
+            <img src="c:\Users\imdee\Downloads\c5.jpeg" alt="Certificate 5">
+            <img src="c:\Users\imdee\Downloads\c6.jpeg" alt="Certificate 6">
+            <img src="c:\Users\imdee\Downloads\c7.jpeg" alt="Certificate 7">
+        </div>
+        <div class="chai-quotes">
+            <h2>Chai or Suraj ❤️</h2>
+            <hr
+                style="color: aqua;width: 100%;height: 3px;justify-content: center;background: linear-gradient(to left,red,yellow); margin-bottom: 20px;">
+            <p>“Come, sit with me… Let’s share chai, some laughs, and maybe a little love too.” ☕✨</p>
+            <p>“Chai toh bahana hai… baat toh tumhare saath waqt bitane ka hai.” ❤️</p>
+            <p>“Chai warms the hands… but you warm the heart.” ❤️☕</p>
+        </div>
+
+    </div>
+
+
+    <div class="education-section">
+        <h2>Education Details</h2>
+
+        <table class="education-table">
+            <tr>
+                <th>Qualification</th>
+                <th>School / College</th>
+                <th>Board / University</th>
+                <th>Year</th>
+                <th>Percentage / CGPA</th>
+            </tr>
+
+            <tr>
+                <td>10th</td>
+                <td>Vidhya varidhi vidyalaya</td>
+                <td>SSC / CBSE</td>
+                <td>2018</td>
+                <td>66%</td>
+            </tr>
+
+            <tr>
+                <td>12th</td>
+                <td>Vidhya varidhi vidyalaya & college</td>
+                <td>HSC / CBSE</td>
+                <td>2020</td>
+                <td>55%</td>
+            </tr>
+
+            <tr>
+                <td>BSc IT</td>
+                <td>Abhinav degree College </td>
+                <td>Mumbai University</td>
+                <td>2025</td>
+                <td>CGPA / 9%</td>
+            </tr>
+        </table>
+    </div>
+
+
+    <div class="education-section">
+        <h2>Get in Touch With Me</h2>
+        <div id="contact" class="contact-section">
+            <h2>Contact Me</h2>
+            <?php if(!empty($success)) { ?>
+            <p style="color: lime; text-align:center; margin-bottom:10px;">
+                <?php echo $success; ?>
+            </p>
+            <?php } elseif(!empty($error)) { ?>
+            <p style="color: red; text-align:center; margin-bottom:10px;">
+                <?php echo $error; ?>
+            </p>
+            <?php } ?>
+            <form method="POST" action="contact.php">
+                <input type="text" name="name" placeholder="Your Name" required>
+                <input type="email" name="email" placeholder="Your Email" required>
+                <textarea name="message" placeholder="Your Message" rows="5" required></textarea>
+                <button type="submit">Send Message</button>
+            </form>
+        </div>
+
+<div id="login" class="login-section">
+    <h2>Login</h2>
+    <?php if(!empty($success)) { ?>
+        <p style="color: lime; text-align:center; margin-bottom:10px;"><?php echo $success; ?></p>
+    <?php } elseif(!empty($error)) { ?>
+        <p style="color: red; text-align:center; margin-bottom:10px;"><?php echo $error; ?></p>
+    <?php } ?>
+    <form method="POST" action="login.php">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button><br>
+        <p style="padding-top: 30px;text-align: center;">☕ Chai With Suraj ❤️ </p>
+    </form>
+</div>
+
+
+        <footer class="footer">
+            <div class="footer-container">
+
+
+                <div class="footer-about">
+                    <h3>Suraj Portfolio</h3>
+                    <p style="color: black;">Frontend Developer specializing in modern UI, responsive design & clean web
+                        experiences.</p>
+                </div>
+
+
+                <div class="footer-links">
+                    <h4>Quick Links</h4>
+                    <a href="#home">Home</a>
+                    <a href="#projects">Projects</a>
+                    <a href="#contact">Contact</a>
+                    <a href="#login">Login</a>
+
+                </div>
+
+                <div class="footer-social">
+                    <h4>Follow Me</h4>
+                    <div class="social-icons">
+                        <a href="https://github.com/Vishwakarmasuraj333"><img
+                                src="https://img.icons8.com/ios-filled/50/ffffff/github.png" /></a>
+                        <a href="http://linkedin.com/in/suraj-vishwakarma-2ab64436a"><img
+                                src="https://img.icons8.com/ios-filled/50/ffffff/linkedin.png" /></a>
+                        <a href="https://www.instagram.com/invites/contact/?utm_source=ig_contact_invite&utm_medium=copy_link&utm_content=mngzxht"
+                            target="_blank"><img
+                                src="https://img.icons8.com/ios-filled/50/ffffff/instagram-new.png" /></a>
+                        <a href="https://www.facebook.com/share/1AWPJbsWya/" target="_blank"><img
+                                src="https://img.icons8.com/ios-filled/50/ffffff/facebook.png" /></a>
+                    </div>
+                </div>
+
+            </div>
+
+            <hr>
+
+            <p class="footer-copy"> ☕❤️ "Chai With Me Love © 2025 Suraj — Frontend Developer Portfolio</p>
+
+
+        </footer>
+        <button id="scrollTopBtn">↑</button>
+        <script src="script.js"></script>
+</body>
+</html>
